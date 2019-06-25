57dde2574e92fc0bca4d415743ad9f7946f61208
tesfanesh <tesfanesh75@gmail.com>
2019-06-24 21:35:16 -0700

    homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,76 @@
+#import the module
+import os
+import csv
 
+
+#define the path for file to open and output file
+filepath = 'budget_data.csv'
+outputpath = 'PyBank_Output.txt'
+
+#Variable declaration
+total_months = 0
+total_revenue = 0
+prev_revenue = 0
+change_list =[]
+greatest_increase = {}
+
+# Read the csv file   
+with open(filepath,newline = "",encoding ="utf-8") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    csv_header = next(csvreader)
+
+    #Read each row
+    for row in csvreader:
+        #Add to Total months
+        total_months + = 1
+
+        #Add to the total revenue
+        total_revenue += int(row[1])
+
+        #Calculate the change in revenue
+        change = int(row[1]) - prev_revenue
+
+        #Add the change in revenue to greatest_increase dictionary
+        greatest_increase[row[0]] = change
+
+        #store the current value of profit/losses to the prev_revenue
+        prev_revenue = int(row[1])
+
+        #Add the change to the change_list list
+        change_list.append(change)
+    
+    #print the values in terminal
+    print("Financial Analysis")   
+    print("----------------------------") 
+    print("Total Months:", total_months)
+    print("Total:","$", total_revenue)
+
+    #calculate the average of the change_list list
+    average = sum(change_list[1:])/len(change_list[1:])
+    print("Average Change:","$",round(average,2))
+    
+#Export the data to text file
+with open(outputpath,"w",encoding ="utf-8") as text_file:
+    text_file.write("Financial Analysis" +"\n")
+    text_file.write("-------------------------" +"\n")
+    text_file.write("Total Months: "+str(total_months) +"\n")
+    text_file.write("Total:"+"$"+str(total_revenue) +"\n")
+    text_file.write("Average Change:"+"$"+str(round(average,2)) +"\n")
+    
+    # calculate the sorted list of greatest_increase dictionary
+    sorted_list = sorted(greatest_increase.values(),reverse = True)
+
+    #Loop through the candidate_count dictionary
+    for key,value in greatest_increase.items():
+
+        #look for the key against the highest value of sorted list in greatest_increase dictionary
+        if sorted_list[0] == value:
+            Greatest = key
+            print("Greatest Increase in Profits:",Greatest,"(","$",value,")")
+            text_file.write("Greatest Increase in Profits:"+ str(Greatest)+"("+"$"+str(value)+")"+"\n")  
+
+        #look for the key against the lowest value of sorted list in greatest_increase dictionary
+        elif sorted_list[-1] == value:
+            smallest = key
+            print("Greatest Decrease in Profits:",smallest,"(","$",value,")")
+            text_file.write("Greatest Decrease in Profits:"+str(smallest)+"("+"$"+str(value)+")"+"\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,73 @@
+#import the module
+import os
+import csv
 
+#define the path for file to open and output file
+csvpath = 'election_data.csv'
+outputpath = 'PyPoll_Output.txt'
+
+#Variable declaration
+count_voters = 0
+candidate_name =[]
+candidate_count ={}
+election_result ={}
+sorted_list = []
+winner =""
+
+# Read the csv file
+with open(csvpath,newline = "") as csvfile:
+   csvreader = csv.reader(csvfile)
+   csv_header = next(csvreader)
+
+   #Read each row
+   for row in csvreader:
+       #Add to count voters
+       count_voters += 1
+       # Save the candidate name in candidatename variable
+       candidatename = row[2]
+
+       # Check if candidate name is not present in the candidate_name list
+       if candidatename  not in candidate_name:
+           # Add the candidate name in candidate_name list
+           candidate_name.append(row[2])
+           # Reset the candidate_count to zero
+           candidate_count[candidatename] = 0
+       #if candidate name is already present add it to the candidate_count
+       candidate_count[candidatename] += 1
+
+# Export the data to text file
+with open(outputpath,"w",encoding ="utf-8") as text_file:
+   #print the results in terminal
+   print("Election Results")
+   print("-------------------------")
+   print("Total Votes: ",count_voters)
+   print("-------------------------")
+
+   #export the result to text file
+   text_file.write("Election Results" +"\n")
+   text_file.write("-------------------------" +"\n")
+   text_file.write("Total Votes: "+str(count_voters) +"\n")
+   text_file.write("-------------------------" +"\n")
+
+   # loop through the candidate_count dictionary and calculate the vote percentage and save it to election_result dictionary
+   for i,j in candidate_count.items():
+       election_result[i] = round(((float(j)/float(count_voters))*100),5)
+       print(i,":",election_result[i] ,"%","(",j,")")
+       text_file.write(str(i)+":"+str(election_result[i])+"%"+"("+str(j)+")"+"\n")
+
+   print("-------------------------")
+   text_file.write("-------------------------" +"\n")
+
+
+   # calculate the sorted list of candidate_count dictionary
+   sorted_list = sorted(candidate_count.values(),reverse = True)
+
+   #Loop through the candidate_count dictionary
+   for key,value in candidate_count.items():
+
+       #look for the key against the value of sorted list in candidate_count dictionary
+       if sorted_list[0] == value:
+           winner = key
+           print("Winner:",winner)
+
+   text_file.write("Winner:" + str(winner))
